02a9d6120c7aaa425e0c45aedfc2f27ec5815352
mudamuda <102901724+AldiAgung@users.noreply.github.com>
2024-01-10 00:58:08 +0700

    satu dua tiga

--- Print.java
+++ Print.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
+
 /**
  * Print
  */
 public class Print {
-
+        Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("halloooooooo");
 
-
+        
         int kunci = 0;
         switch (kunci) {
             case 1:
