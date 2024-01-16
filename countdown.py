835498c1cf83ee01a730bebd86dbf5318fbf7e81
mudamuda <102901724+AldiAgung@users.noreply.github.com>
2024-01-16 23:56:09 +0700

    updateeeeee

--- countdown.py
+++ countdown.py
@@ -2,13 +2,15 @@ import os
 import time
 
 pilihan = input("Apakah tetap ingin melanjutkan? (Ya/Tidak)")
-if pilihan == "Ya":
+kata = pilihan.upper()
+
+if kata == "YA":
     for i in range(10,0,-1):
         print("Komputer mati dalam "+ str(i))
-        time.sleep(2)
+        time.sleep(1)
     if i <= 1:
-        os.system("shutdown /r /t 2")
-        # print("haloooooooooo")
+        # os.system("shutdown /r /t 2")
+        print("haloooooooooo")
         print("baiiii")
-    elif pilihan == "Tidak":
+    elif kata == "TIDAK":
         exit()
