51bb21dd3df9123c9af8fc20f004e9d46285d5b7
mudamuda <102901724+AldiAgung@users.noreply.github.com>
2024-01-24 23:18:05 +0700

    tambahan baru

--- countdown.py
+++ countdown.py
@@ -1,16 +1,44 @@
 import os
 import time
+import sys 
+import random
 
 pilihan = input("Apakah tetap ingin melanjutkan? (Ya/Tidak)")
 kata = pilihan.upper()
 
+
 if kata == "YA":
-    for i in range(10,0,-1):
+    for i in range(5,0,-1):
         print("Komputer mati dalam "+ str(i))
         time.sleep(1)
-    if i <= 1:
-        # os.system("shutdown /r /t 2")
-        print("haloooooooooo")
-        print("baiiii")
-    elif kata == "TIDAK":
-        exit()
+        if i <= 1:
+            print("tapi aku masih bakalan kasih kesempatan satu kali lagi")
+            time.sleep(2)
+            print("jika bener jawabannya, gabakalan ke restart")
+            time.sleep(2)
+            print("kalau salah yah tetep aja ke restart lmao")
+            time.sleep(2)
+            print("siap?")
+            time.sleep(2)
+            tebak = input("tebak angkanya: ")
+            angka = random.randrange(1,10)
+            if (tebak != angka):
+                print("sayang sekali jawaban kamu salah")
+                time.sleep(2)
+                print("jawabannya adalah: "+ str(angka))
+                time.sleep(2)
+                print("jadi sekarang pc kamu ke restart deh, hahahhaha")
+                time.sleep(2)
+                # os.system("shutdown /r /t 2")
+                berkas = open('Halopy', 'w')
+                berkas.write("laptop kamu mati hahah")
+                print("haloooooooooo")
+                print("baiiii")
+            else:
+                print("jawaban yang benar adalah: "+ str(angka))
+                print("selamat pc kammu tidak ke restart")
+                time.sleep(2)
+                print("nikmati waktunya :)")
+                exit()
+elif kata == "TIDAK":
+    exit()
