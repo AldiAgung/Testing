a3742d7bfcc7f05eb6034f0b492b275ad42727be
mudamuda <102901724+AldiAgung@users.noreply.github.com>
2024-06-21 22:29:30 +0700

    haha baru

--- laptop.py
+++ laptop.py
@@ -1,4 +1,7 @@
 import os
 
-kata =  int(input("berapa lama?: "))
-os.system('shutdown /s /t '+ str(kata))
+try:
+    kata =  int(input("berapa lama?: "))
+    os.system('shutdown /s /t '+ str(kata))
+except ValueError:
+    print(f"Harus berupa sebuah angka")
