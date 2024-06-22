c79ce5390e07fd0962d99bbb52a8706e5c410407
mudamuda <102901724+AldiAgung@users.noreply.github.com>
2024-06-22 13:49:59 +0700

    baru

--- laptop.py
+++ laptop.py
@@ -1,7 +1,12 @@
 import os
+import time
 
 try:
     kata =  int(input("berapa lama?: "))
     os.system('shutdown /s /t '+ str(kata))
 except ValueError:
     print(f"Harus berupa sebuah angka")
+    time.sleep(2)
+
+    #python -m compileall -l laptop.py
+    #python -m PyInstaller laptop.py
