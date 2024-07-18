4801a57714f501e7171dcc19b10ede560643a7b4
mudamuda <102901724+AldiAgung@users.noreply.github.com>
2024-07-18 13:59:07 +0700

    Baru updaet

--- rill/app.py
+++ rill/app.py
@@ -1,21 +1,23 @@
 from tkinter import *
 import tkinter as tk
-from ttkbootstrap.constants import *
 # from tkFileDialog import *
-# from server import hubungkan
+from server import status, hubungkan
 
 # port = 5050
 m = Tk()
 m.geometry('800x600')
 m.title("Hallooooo")
-status = Label(m, text= 'Status internet: ').grid(row = 0)
-tombol = Button(m, text= 'sentuh aku', command = m.destroy) # , width= 25, command= m.get(hubungkan))
-box1 = Entry(m)
+m.frame()
+
+Ip_address = Label(m, text= 'Ip Internett: ' {}).grid(row = 0)
+status_internet = Label(m, text= 'Status internet: ').grid(row = 1)
+tombol = Button(m, text= 'refresh', command = m.destroy) # , width= 25, command= hubungkan()))
+# status_internet = print(status)
 
 # Peletakan tombol dan semacamnya #
 # tombol.grid(column= 0, row= 0)
 tombol.grid(row = 1, column= 1)
-box1.grid(row = 0, column= 1)
+# status_internet.grid(row = 0, column= 1)
 
 # tombol.pack()
 # namalabel.pack()
---
+++ rill/server.py
@@ -0,0 +1,31 @@
+import socket, sys
+
+port = 5050
+def hubungkan():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("terbuat socket")
+    except socket.timeout as err:
+        print("Tidak bisa dijalankan karena: %s" %(err))
+        # connecting to the server
+        host_ip = socket.gethostbyname('www.google.com')
+        s.connect(host_ip, port)
+        s.send(1024)
+    sys.exit()
+
+def alamatip():
+    try:
+        namahost = socket.gethostbyname()
+        ipaddress = socket.gethostbyname(namahost)
+        return ipaddress
+    except socket.gaierror as gada:
+        print(f'Tidak koneksi internet yang valid {gada}')
+
+# def status():
+#     if hubungkan() == True:
+#         print(" server online")
+#     else:
+#         print("server offline")
+
+alamatip()
+hubungkan()
