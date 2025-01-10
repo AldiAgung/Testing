70130d99286ab72965612ba564e04b041c26d0c2
mudamuda <102901724+AldiAgung@users.noreply.github.com>
2025-01-11 01:16:51 +0700

    lupa dehhh

--- rill/app.py
+++ rill/app.py
@@ -1,46 +1,42 @@
 from tkinter import *
-# from tkFileDialog import *
-from server import hubungkan, alamatip
+from tkinter import messagebox
+from ip_grab import alamatip
 import os
 from datetime import datetime
 
 #inisiasi variable
-# port = 5050
 m = Tk()
 # m.geometry('800x600')
 m.title("Hallooooo")
 m.frame()
 m.grid()
 waktu_lokal = datetime.now().strftime('%H:%M:%S')
-waktu_awal = waktu_lokal
 
-# def convert(nama):
-#     alamatip.__name__
-
-# def kembali(self):
-#     d
-
+# Fungsi #
 def refresh():
     m.destroy()
-    os.popen(__name__)
+    os.system("python " + __file__)
 
-def update(mystring):
-    update.set(mystring) 
+def lihatip():
+    try:
+        alamat = alamatip()
+        if "Error" in alamat:
+            raise Exception(alamat)
+        label_ip.config(text= f" {alamat}")
+    except Exception as e:
+        messagebox.showerror("Terjadi kesalahan", str(e))
+
 
 # inisiasi label dll
-label_ip = Label(m, text= 'Ip Internet: '+ str(alamatip.__name__)).grid(row = 0, column= 0)
-status_internet = Label(m, text= 'Status internet: ').grid(row = 2, column= 0)
-waktu_awal = Label(m, text= 'Waktu sekarang '+ str(waktu_lokal)).grid(row= 1, column= 0)
-waktu_akhir = Label(m, text= 'Tere')
-tombol_refresh = Button(m, text= "Refresh", command= refresh)
-# tombol_refresh.bind("<Button-1>", convert)
-# updatehasil = StringVar(m)
-# status_internet = print(status)
+Label(m, text= 'Ip Internet: ').grid(row = 0, column= 0, pady = 5, padx= 5, sticky='w')
+label_ip = Label(m, text= "")
+label_ip.grid(row=0, column=1, pady=5, padx= 5)
+# status_internet = Label(m, text= 'Status internet: '+ ).grid(row = 2, column= 0)
+waktu_lokal = Label(m, text= f"Waktu lokal: {waktu_lokal}").grid(row= 1, column= 0, columnspan= 2, padx= 5, pady= 5, sticky= 'w')
 
-# Peletakan tombol dan semacamnya #
-tombol_refresh.grid(row = 3, column= 0)
-# tombol.pack()
-# namalabel.pack()
+## Tombol ##
+tombol_refresh = Button(m, text= "Refresh", command= refresh).grid(row = 2, column= 0, columnspan= 2, pady= 10)
 
 if __name__ == "__main__":
+    lihatip()
     m.mainloop()
